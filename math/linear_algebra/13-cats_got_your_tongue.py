ff3edebadfcd80aa5a0a49fc0f78ed1a7321b2ab
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 04:15:39 -0800

    13

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""a function that concats 2 matrix"""
+def np_cat(mat1, mat2, axis=0):
+    """returns concatenated matrix"""
+    return np.concatenate((mat1,mat2), axis)
