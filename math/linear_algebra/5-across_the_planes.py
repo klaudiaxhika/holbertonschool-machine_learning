d8b6b2b04d3b3b34fab82894b86922f6ada7eef8
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 02:34:11 -0800

    5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,8 +8,8 @@ def add_matrices2D(mat1, mat2):
     if len(mat1[0]) != len(mat2[0]) or len(mat1) != len(mat2):
         return None
     for i in range(len(mat1)):
-            row = []
-            for j in range(len(mat1[0])):
-                row.append(mat1[i][j] + mat2[i][j])
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
             mat.append(row)
     return mat
