c4e89a1aef90d4a015baad98459d681cc2af0076
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 02:32:30 -0800

    5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,11 +4,12 @@
 
 def add_matrices2D(mat1, mat2):
     """returns sum  of a matrix"""
-    result = []
-    if [len(mat1), len(mat1[0])] != [len(mat2), len(mat2[0])]:
+    mat = []
+    if len(mat1[0]) != len(mat2[0]) or len(mat1) != len(mat2):
         return None
     for i in range(len(mat1)):
-        result.append([])
-        for j in range(len(mat1)):
-            result[i].append(mat1[i][j]+mat2[i][j])
-    return result
+            row = []
+            for j in range(len(mat1[0])):
+                row.append(mat1[i][j] + mat2[i][j])
+            mat.append(row)
+    return mat
