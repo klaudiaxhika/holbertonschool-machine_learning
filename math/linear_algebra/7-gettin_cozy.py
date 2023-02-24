6b4b60fd4117c6697a885eee814f5319504aeaeb
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 02:03:41 -0800

    7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,18 +2,18 @@
 """ a function that returns concat of matrix"""
 
 
-def cat_matrices2D(mat1, mat2, axis = 0):
+def cat_matrices2D(mat1, mat2, axis=0):
     """returns concat matrix"""
     result = []
     if (axis == 0):
         if len(mat1[0]) != len(mat2[0]):
             return None
-        return mat1 + mat2;
+        return mat1 + mat2
 
     if(axis == 1):
         if len(mat1) != len(mat2):
             return None
-        for i in range (len(mat1)):
+        for i in range(len(mat1)):
             result.append(mat1[i] + mat2[i])
 
     return result
