0ab320223082e4206052f64b211db74e24bfeea1
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 01:46:53 -0800

    4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
+"""a function that returns sum of arrays"""
+
+
 def add_arrays(arr1, arr2):
+    """returns sum of arrays"""
     result=[]
     if len(arr1) != len(arr2):
         return None
     for i in range(len(arr1)):
         result.append(arr1[i]+arr2[i])
     return result
-
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
