441b9d329fc81e3842646fb420ac297ed2b040a1
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 04:25:58 -0800

    14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""a function performs matrix multiplication"""
+def np_matmul(mat1, mat2):
+    """returns multiplied matrix"""
+    return np.dot(mat1, mat2)
