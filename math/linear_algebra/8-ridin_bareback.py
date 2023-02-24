19da84e4bb7b0855afa713996897d62cc7748f9e
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-24 02:07:35 -0800

    8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -6,7 +6,7 @@ def mat_mul(mat1, mat2):
     """returns multiplicated matrix"""
     if len(mat1[0]) != len(mat2):
         return None
-    mat = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))] 
+    mat = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             product = 0
