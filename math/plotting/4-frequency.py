87c27ec16e8d9ff390d439890c0b95d17ee095ed
klaudiaxhika <klaudia.xhika@softup.co>
2023-02-28 03:29:46 -0800

    4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
+binwidth=10
 
-plt.hist(student_grades, edgecolor='black', bins=10)
+plt.hist(student_grades, edgecolor='black', bins=range(0, 100 + binwidth, binwidth))
 plt.xlim(0,100)
 plt.ylim(0,30)
 plt.xlabel('Grades')
