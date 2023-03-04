547eef7ce6c15f0f87da63aa17d052bdf20e7075
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-04 12:50:48 -0800

    10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,5 +6,7 @@ def poly_derivative(poly):
     """returns the coeficents"""
     der_list = []
     for power, coeff in enumerate(poly):
+        if(power == 0):
+            continue
         der_list.append(power * coeff)
     return der_list
