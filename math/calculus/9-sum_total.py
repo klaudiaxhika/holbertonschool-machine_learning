ff9381b1f64486d5b2814c7273615f5da3f8c600
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-04 11:46:33 -0800

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(m):
     """returns squared sum"""
-    if isinstance(m, (int,float)):
+    if isinstance(m, (int, float)):
         return int(m*(m+1)*(2*m+1)/6)
     return None
