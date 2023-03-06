850d6e334e53773c1c088a2f7367d3d773095b75
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-06 08:40:07 -0800

    17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" a function that calculates the integral"""
+
+
+def poly_integral(poly, C=0):
+    """returns the integral"""
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    elif type(C) is int:
+        if poly == [0]:
+            return C
+        exponent = 0
+        integral = poly.copy()
+        for i in range(len(integral)):
+            if type (integral[i]) is int or type (integral[i]) is float:
+                exponent += 1
+                number = integral[i] / exponent
+                integral[i] = int(number) if number % 1 == 0 else number
+            else:
+                return None
+        integral.insert(0,C)
+        return integral
