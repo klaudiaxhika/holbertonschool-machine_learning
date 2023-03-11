a113773ae9fdf6fddc77a3ce623127014a76f76d
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-11 14:28:08 -0800

    2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,7 +19,7 @@ class Poisson:
 
     def pmf(self, k):
         """
-        a class Poisson that represents a poisson distribution:
+        Calculates the value of the PMF for a given number of successes
         """
         if type(k) is not int:
             k = int(k)
@@ -32,3 +32,16 @@ class Poisson:
             factorial *= (i + 1)
         pmf = ((lambtha ** k) * (e** -lambtha)) / factorial
         return pmf
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes
+	"""
+	if type(k) is not int:
+	    k = int(k)
+	if k < 0:
+	    return 0
+	cdf = 0
+	for i in range(k + 1):
+	    cdf += self.pmf(i)
+	return cdf
