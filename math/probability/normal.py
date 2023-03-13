82f81e64f08d2d2f37fb66ce220c0b81f3f26787
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-13 10:22:24 -0700

    6

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -9,13 +9,13 @@ class Normal:
     def __init__(self, data=None, mean=0., stddev=1.):
         self.mean = float(mean)
         self.stddev = float(stddev)
-        if stddev <= 0:
+        if self.stddev <= 0:
             raise ValueError("stddev must be a positive value")
-        if type(data) is not list:
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
         if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("must contain multiple values")
             self.mean = float(sum(data) / len(data))
             sumStddev = 0
             for i in data:
