5b6a007be0d9d24c19af137327f52492265682a1
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-20 10:08:09 -0700

    4

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,2 +1,42 @@
+#!/usr/bin/env python3
+import numpy as np
 
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution with channels on grayscale images
+    """
+    m, height, width, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if (padding == 'same'):
+        if (kh % 2) == 1 and (kw % 2) == 1:
+            ph = (kh - 1) // 2
+            pw = (kw - 1) // 2
+        else:
+            ph = kh // 2
+            pw = kw // 2
+    elif (padding == 'valid'):
+        ph = 0
+        pw = 0
+    else:
+        ph, pw = padding
+        
+        
+    ch = ((height + 2*ph - kh) // sh) + 1
+    cw = ((width + 2*pw - kw) // sw) + 1
+    
+    img_padded = np.pad(images, ((0,0), (ph,ph), (pw,pw), (0,0)), mode='constant')
+    convoluted = np.zeros((m, ch, cw))
+    
+    i = 0
+    for h in range(0, height + 2*ph - kh + 1, sh):
+        j = 0
+        for w in range(0, width + 2*pw - kw +1, sw):
+            output = np.sum(img_padded[:, h: h + kh, w: w + kw, :] * kernel,
+                            axis=1).sum(axis=1).sum(axis=1)
+            convoluted[:, i, j] = output
+            j += 1
+        i += 1
+    return convoluted
 
