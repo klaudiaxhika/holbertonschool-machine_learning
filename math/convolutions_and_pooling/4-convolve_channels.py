fa37a4a5df0d2f2ba4a306e9a9d8c1707501ff8e
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 11:55:38 +0100

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Imports numpy"""
+
 import numpy as np
+"""Performs a same convolution on grayscale images"""
+
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """
@@ -21,22 +25,17 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         pw = 0
     else:
         ph, pw = padding
-        
-        
     ch = ((height + 2*ph - kh) // sh) + 1
     cw = ((width + 2*pw - kw) // sw) + 1
-    
-    img_padded = np.pad(images, ((0,0), (ph,ph), (pw,pw), (0,0)), mode='constant')
+    img_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
     convoluted = np.zeros((m, ch, cw))
-    
     i = 0
     for h in range(0, height + 2*ph - kh + 1, sh):
         j = 0
-        for w in range(0, width + 2*pw - kw +1, sw):
+        for w in range(0, width + 2*pw - kw + 1, sw):
             output = np.sum(img_padded[:, h: h + kh, w: w + kw, :] * kernel,
                             axis=1).sum(axis=1).sum(axis=1)
             convoluted[:, i, j] = output
             j += 1
         i += 1
     return convoluted
-
