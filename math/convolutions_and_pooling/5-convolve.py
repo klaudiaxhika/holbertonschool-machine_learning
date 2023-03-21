7b8cedd9fd9d78ebcc7f0f0ea26645123335af2b
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 17:45:02 +0100

    Update 5-convolve.py

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -6,33 +6,41 @@ import numpy as np
 
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """
-    Performs convolution on grayscale images
-    """
-    m, height, width, c = images.shape
-    kh, kw, c, nc = kernels.shape
-    sh, sw = stride
-    if (padding == 'same'):
-        ph = ((height - 1) * sh + kh - height) // 2 + 1
-        pw = ((width - 1) * sw + kw - width) // 2 + 1
-    elif (padding == 'valid'):
-        ph = 0
-        pw = 0
+    # Get dimensions of input images and kernels
+    input_shape = images.shape
+    kernel_shape = kernels.shape
+    
+    # Check that input is valid
+    if input_shape[-1] != kernel_shape[-2]:
+        raise ValueError("Last dimension of input images must match second-to-last dimension of kernels.")
+    
+    # Determine padding values
+    if padding == 'same':
+        # Calculate amount of padding required to preserve input shape
+        pad_h = int(np.ceil(((input_shape[1] - 1) * stride[0] + kernel_shape[0] - input_shape[1]) / 2))
+        pad_w = int(np.ceil(((input_shape[2] - 1) * stride[1] + kernel_shape[1] - input_shape[2]) / 2))
+    elif padding == 'valid':
+        pad_h = pad_w = 0
     else:
-        ph, pw = padding
-    ch = ((height + 2*ph - kh) // sh) + 1
-    cw = ((width + 2*pw - kw) // sw) + 1
-    img_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        mode='constant')
-    convoluted = np.zeros((m, ch, cw, c))
-    for k in range(nc):
-        i = 0
-        for h in range(0, height + 2*ph - kh + 1, sh):
-            j = 0
-            for w in range(0, width + 2*pw - kw + 1, sw):
-                output = np.sum(img_padded[:, h: h + kh, w: w + kw, :]
-                                * kernels[:, :, :, k], axis=(1, 2))
-                convoluted[:, i, j, :] = output
-                j += 1
-            i += 1
-    return convoluted
+        raise ValueError("Padding must be 'same' or 'valid'.")
+    
+    # Pad input images
+    images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), 'constant')
+    
+    # Determine output shape
+    output_h = int(np.floor((input_shape[1] + 2 * pad_h - kernel_shape[0]) / stride[0] + 1))
+    output_w = int(np.floor((input_shape[2] + 2 * pad_w - kernel_shape[1]) / stride[1] + 1))
+    output_shape = (input_shape[0], output_h, output_w, kernel_shape[-1])
+    
+    # Initialize output array
+    output = np.zeros(output_shape)
+    
+    # Perform convolution for each kernel
+    for i in range(kernel_shape[-1]):
+        kernel = kernels[..., i]
+        for j in range(output_h):
+            for k in range(output_w):
+                output[:, j, k, i] = np.sum(images[:, j*stride[0]:j*stride[0]+kernel_shape[0],
+                                                      k*stride[1]:k*stride[1]+kernel_shape[1], :] * kernel, axis=(1, 2, 3))
+    
+    return output
