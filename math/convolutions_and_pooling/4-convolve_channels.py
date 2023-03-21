2f4c2db4b4a4c5d3fe9579de0264492f8a8b51d0
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 12:38:54 +0100

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -12,14 +12,9 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     m, height, width, c = images.shape
     kh, kw, kc = kernel.shape
     sh, sw = stride
-
     if (padding == 'same'):
-        if (kh % 2) == 1 and (kw % 2) == 1:
-            ph = (kh - 1) // 2
-            pw = (kw - 1) // 2
-        else:
-            ph = kh // 2
-            pw = kw // 2
+        ph = ((height - 1) * sh + kh - height) // 2 + 1
+        pw = ((width - 1) * sw + kw - width) // 2 + 1
     elif (padding == 'valid'):
         ph = 0
         pw = 0
