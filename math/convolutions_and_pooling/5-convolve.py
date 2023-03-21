8142107580154763781989514b99b22a207cb150
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 12:28:47 +0100

    Update 5-convolve.py

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python3
+"""Imports numpy"""
+
+import numpy as np
+"""Performs a same convolution on grayscale images"""
 
 
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+  
