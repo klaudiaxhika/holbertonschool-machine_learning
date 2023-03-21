31c237bfc77c876e4fe66fd625ffcf686293a1ad
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 11:58:34 +0100

    Update 6-pool.py

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,2 +1,33 @@
+#!/usr/bin/env python3
+"""Imports numpy"""
+
+import numpy as np
+"""Performs a same convolution on grayscale images"""
 
 
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+    Performs pooling on grayscale images
+    """
+    m, height, width, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+        
+        
+    pool_h = ((height - kh) // sh) + 1
+    pool_w = ((width - kw) // sw) + 1
+    
+    pooled = np.zeros((m, pool_h, pool_w, c))
+    
+    i = 0
+    for h in range(0, height - kh + 1, sh):
+        j = 0
+        for w in range(0, width - kw + 1, sw):
+            if mode == 'max':
+                output = np.max(images[:, h + kh, w: w + kw, :], axis=(1, 2))
+            if mode == 'avg':
+                output = np.average(images[:, h + kh, w: w + kw, :], axis=(1, 2))
+            pooled[:, i, j, :] = output
+            j += 1
+        i += 1
+    return pooled
