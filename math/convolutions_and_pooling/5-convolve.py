c3bef8abaca5dec1d2d3206800362be7f1f6f7d3
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-21 15:09:10 +0100

    Update 5-convolve.py

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -25,16 +25,13 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     img_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                         mode='constant')
     convoluted = np.zeros((m, ch, cw, c))
-    convoluted_1 = np.zeros((m, ch, cw, c))
-    convoluted_2 = np.zeros((m, ch, cw, c))
-    
     for k in range(nc):
-        i = 0                            
+        i = 0
         for h in range(0, height + 2*ph - kh + 1, sh):
             j = 0
             for w in range(0, width + 2*pw - kw + 1, sw):
-                output = np.sum(img_padded[:, h: h + kh, w: w + kw, :] * kernels[k],
-                                axis=(1,2))
+                output = np.sum(img_padded[:, h: h + kh, w: w + kw, :] 
+                                * kernels[k], axis=(1, 2))
                 convoluted[:, i, j, :] = output
                 j += 1
             i += 1
