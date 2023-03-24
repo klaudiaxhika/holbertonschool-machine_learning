30cf97c20023573c8432a192e178b06980dc956f
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-24 02:54:29 -0700

    1

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -4,15 +4,15 @@
 import numpy as np
 """calculates a correlation matrix"""
 
+
 def correlation(C):
     """
     calculates a correlation matrix
     """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if len(C) < 2 or (C.shape[0] != C.shape[1]):
+    if C.ndim != 2 or (C.shape[0] != C.shape[1]):
         raise ValueError("C must be a 2D square matrix")
-    
     D = np.sqrt(np.diag(C))
     correlation_matrix = np.outer(D, D)
     correlation_matrix[C != 0] = C[C != 0] / correlation_matrix[C != 0]
