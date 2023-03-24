35fa5c544b724c38363cdbc2cc71b3ad685c1d66
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-24 02:32:33 -0700

    0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -9,12 +9,12 @@ def mean_cov(X):
     """
     calculates the mean and covariance of a data set
     """
-    if not isinstance(X,np.ndarray):
+    if not isinstance(X, np.ndarray):
         raise TypeError("X must be a 2D numpy.ndarray")
     if len(X) < 2:
         raise ValueError("X must contain multiple data points")
     n, d = X.shape
-    mean = np.mean(X, axis = 0)
+    mean = np.mean(X, axis=0)
     cov = np.zeros((d, d))
     for i in range(n):
         cov += np.outer((X[i] - mean), (X[i] - mean))
