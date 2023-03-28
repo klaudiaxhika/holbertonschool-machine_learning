f8fcc9aab205050e7bbe90560fac7106ecd64fbb
klaudiaxhika <klaudia.xhika@softup.co>
2023-03-28 05:54:20 -0700

    0

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Import numpy"""
+
+import numpy as np
+"""calculates a likelihood"""
+
+
+def likelihood(x, n, P):
+    """
+    calculates the likelihood
+    """
+    factorial = np.math.factorial
+    fact_coeff = factorial (n) / (factorial (n-x) * factorial(x)
+    likelihood = fact_coeff * (P**x) * ( (1 - P) ** (n -X))
+    return likelihood
