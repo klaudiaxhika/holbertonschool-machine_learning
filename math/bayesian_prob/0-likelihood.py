3cc7fc75d4357a60dc9c959e20e4e2f173404b94
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-29 21:57:31 +0200

    Update 0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -7,9 +7,22 @@ import numpy as np
 
 def likelihood(x, n, P):
     """
-    calculates the likelihood
+    calculates likelihood
     """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    for value in P:
+        if value > 1 or value < 0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    # likelihood calculated as binomial distribution
     factorial = np.math.factorial
-    fact_coeff = factorial(n) / (factorial(n-x) * factorial(x))
-    likelihood = fact_coeff * (P**x) * ((1 - P) ** (n -X))
+    fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
     return likelihood
