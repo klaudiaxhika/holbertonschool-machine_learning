77513cd9646f0eaff3b4590b16a523c2f79d3f20
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-03-29 21:54:25 +0200

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -24,21 +24,20 @@ class MultiNormal:
 
         self.cov = np.matmul(centered, centered.T) / (n - 1)
 
-
     def pdf(self, x):
         """
-        return the pdf 
+        return the pdf
         """
         if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
         if len(x.shape) != 2:
             raise ValueError("x must have the shape ({}, 1)".format(d))
-        
+
         test_d, one = x.shape
         if test_d != d or one != 1:
             raise ValueError("x must have the shape ({}, 1)".format(d))
-        
+
         det = np.linalg.det(self.cov)
         inv = np.linalg.inv(self.cov)
         pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
