2a1af74b224065f94e7ad930fd635c437cdcb1de
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-04-18 23:53:06 +0200

    Create 0-neuron.py

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+class Neuron:
+    """
+    class that represents a single neuron performing binary classification
+    """
+
+    # TASK 0
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
