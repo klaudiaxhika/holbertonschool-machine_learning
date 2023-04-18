e2c9804e5254d97a195158f01cc16b2192ec3454
klaudiaxhika <klaudia.xhika@softup.co>
2023-04-19 00:04:46 +0200

    1

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -10,24 +10,23 @@ class Neuron:
     class that represents a single neuron performing binary classification
     """
 
-    # TASK 1
-        def __init__(self, nx):
-            if type(nx) is not int:
-                raise TypeError("nx must be an integer")
-            if nx < 1:
-                raise ValueError("nx must be a positive integer")
-            self.__W = np.random.randn(1, nx)
-            self.__b = 0
-            self.__A = 0
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
-        @property
-        def W(self):
-            return (self.__W)
+    @property
+    def W(self):
+        return (self.__W)
 
-        @property
-        def b(self):
-            return (self.__b)
+    @property
+    def b(self):
+        return (self.__b)
 
-        @property
-        def A(self):
-            return (self.__A)
+    @property
+    def A(self):
+        return (self.__A)
