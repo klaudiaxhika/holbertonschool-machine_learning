3d9125de930f8c517f0410de7518e1be48a3b2f9
klaudiaxhika <klaudia.xhika@softup.co>
2023-04-21 22:09:03 +0200

    16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -31,7 +31,8 @@ class DeepNeuralNetwork:
                 raise TypeError("layers must be a list of positive integers")
 
             weights["b{}".format(index)] = np.zeros((layer, 1))
-            weights["W{}".format(index)] = (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            weights["W{}".format(index)] = (
+                np.random.randn(layer, previous) * np.sqrt(2 / previous))
             previous = layer
 
         self.L = len(layers)
