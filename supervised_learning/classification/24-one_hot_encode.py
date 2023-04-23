ea338ab15da0c3b3d186100f111d77710702bc71
klaudiaxhika <klaudia.xhika@softup.co>
2023-04-23 22:49:09 +0200

    24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -7,7 +7,8 @@ def one_hot_encode(Y, classes):
     """
     converts a numeric label vector into a one-hot matrix
     """
-    if not isinstance(Y, np.ndarray) or Y.ndim != 1 or not isinstance(classes, int) or classes <= 0:
+    if not isinstance(Y, np.ndarray) or Y.ndim != 1 or not isinstance(classes, int) or classes <= 0 \
+        or classes < np.max(Y):
         return None
     m = Y.shape[0]
     one_hot = np.zeros((classes, m))
