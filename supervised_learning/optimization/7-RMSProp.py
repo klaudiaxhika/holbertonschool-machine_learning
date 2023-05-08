f0969ac1ab31882e620a4cc022cddc7431d9a986
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-08 23:47:32 +0200

    Create 7-RMSProp.py

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    update_variables_RMSProp
+    """
+    s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    var -= alpha * (grad / (epsilon + (s_dW ** (1 / 2))))
+    return var, s_dW
