3ed81dc478ba038534fcd1f284d3e783fee22034
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-08 22:09:42 +0200

    Create 1-sensitivity.py

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Imports numpy"""
+
+
+import numpy as np
+"""Calculates a sensitivity matrix"""
+
+
+def sensitivity(confusion):
+    tp = np.diag(confusion)
+    fn = np.sum(confusion, axis=1) - tp
+    
+    return tp / (tp + fn)
+  
