c9dcaf5baf2256ae3d5665d4a02fd98aa88bff7d
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-08 22:42:17 +0200

    Update 1-sensitivity.py

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Imports numpy"""
 
-import numpy as np
 
-"""Calculates a sensitivity matrix"""
+import numpy as np
 
 
 def sensitivity(confusion):
+    """
+    Returns a sensitivity matrix
+    """
     tp = np.diag(confusion)
     fn = np.sum(confusion, axis=1) - tp
 
