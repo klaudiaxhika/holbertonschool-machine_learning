b6edf4d1fad973a2c83761f851c25fd77ac9cb85
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-08 23:37:12 +0200

    Update 4-moving_average.py

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""
+Imports numpy
+"""
 
+
+import numpy as np
+
+
+def moving_average(data, beta):
+    """
+    shuffle_data
+    """
+    v = 0
+    EMA = []
+    for i in range(len(data)):
+        v = ((v * beta) + ((1 - beta) * data[i]))
+        EMA.append(v / (1 - (beta ** (i + 1))))
+    return EMA
