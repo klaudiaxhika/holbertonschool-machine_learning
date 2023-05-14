d71b1c004239da9171d17cfc8f5abe81854cc332
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-14 19:49:06 +0200

    Update 8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -4,10 +4,12 @@
 
 import tensorflow as tf
 
+
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """
     create_RMSProp_op
     """
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
