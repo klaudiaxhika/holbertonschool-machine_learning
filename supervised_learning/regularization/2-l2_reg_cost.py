15fe7108c66b7b8cb84746c1e5c1e60c4944e317
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-15 21:53:30 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -9,17 +9,5 @@ def l2_reg_cost(cost, lambtha):
     """
     Calculates the cost of a neural network with L2 regularization
     """
-    with tf.name_scope('l2_reg_cost'):
-        # Get all the trainable variables in the graph
-        trainable_vars = tf.trainable_variables()
-        
-        # Calculate the L2 regularization term for each variable
-        l2_reg_terms = [tf.nn.l2_loss(var) for var in trainable_vars]
-        
-        # Add up the L2 regularization terms
-        l2_reg = tf.add_n(l2_reg_terms)
-        
-        # Add the L2 regularization term to the cost
-        cost_with_l2_reg = tf.add(cost, lambtha * l2_reg, name='cost_with_l2_reg')
-        
-        return cost_with_l2_reg
+    l2_reg_cost = tf.losses.get_regularization_losses()
+    return (cost + l2_reg_cost)
