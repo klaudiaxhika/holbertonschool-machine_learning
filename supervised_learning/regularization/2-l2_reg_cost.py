99eb884476c0c608e22d6af368df72927c3e73c9
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-15 21:28:59 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -9,5 +9,5 @@ def l2_reg_cost(cost, lam):
     """
     l2_reg_cost
     """
-    l2_reg_cost = tf.losses.get_regularization_losses()
-    return (cost + l2_reg_cost)
+    l2_reg_losses = sum(tf.nn.l2_loss(w) for w in model.trainable_variables)
+    return cost + lam * l2_reg_losses
