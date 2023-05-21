35f0bcf4064049e5e9ce61dd7e0ef87a801d9253
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-05-21 20:54:15 +0200

    Create 11-config.py

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Imports keras"""
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """saves config"""
+    json = network.to_json()
+    with open(filename, 'w+') as f:
+        f.write(json)
+    return None
+
+
+def load_config(filename):
+    """loads config"""
+    with open(filename, 'r') as f:
+        json_string = f.read()
+    model = K.models.model_from_json(json_string)
+    return model#!/usr/bin/env python3
