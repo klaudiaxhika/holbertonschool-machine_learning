7f2f7bd9eca0b6cce37abf14cfe23f09e5412758
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-06-20 00:18:47 +0200

    Update 7-densenet121.py

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -5,8 +5,8 @@ Defines a function that builds a DenseNet-121 network using Keras model
 
 
 import tensorflow.keras as K
-dense_block = dense_block
-transition_layer = transition_layer
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transition_layer').transition_layer
 
 
 def densenet121(growth_rate=32, compression=1.0):
