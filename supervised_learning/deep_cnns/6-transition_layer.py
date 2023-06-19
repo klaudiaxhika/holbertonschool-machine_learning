5ad280eb352de99bea3fa60fcddcdaa3afb43aaa
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-06-20 00:24:14 +0200

    Update 6-transition_layer.py

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -30,23 +30,23 @@ def transition_layer(X, nb_filters, compression):
         the output of transition layer and
             the number of filter within the output
     """
-    
+
     init = K.initializers.he_normal()
     activation = K.activations.relu
-    
+
     Batch_Norm1 = K.layers.BatchNormalization(axis=3)(X)
     ReLU1 = K.layers.Activation(activation)(Batch_Norm1)
-    
+
     nb_filters *= compression
     nb_filters = int(nb_filters)
-    
+
     C1 = K.layers.Conv2D(filters=nb_filters,
                          kernel_size=(1, 1),
                          padding='same',
                          kernel_initializer=init)(ReLU1)
-    
+
     AP1 = K.layers.AveragePooling2D(pool_size=(2, 2),
                                     strides=(2, 2),
                                     padding="valid")(C1)
-    
+
     return AP1, nb_filters
