02e48e8f0481eae8985cc956b9b5550b11245924
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-06-21 17:55:05 +0200

    Update 1-yolo.py

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -20,32 +20,37 @@ class Yolo:
         class_names = [name.strip() for name in class_names]
         return class_names
 
-   def process_outputs(self, outputs, image_size):
-    boxes = []
-    box_confidences = []
-    box_class_probs = []
-
-    for output in outputs:
-        grid_height, grid_width, num_anchors, _ = output.shape
-
-        # Extract bounding box coordinates
-        box = output[..., :4]
-        box[..., :2] = self.sigmoid(box[..., :2])
-        box[..., 2:] = np.exp(box[..., 2:]) * self.anchors / np.array([grid_width, grid_height])
-        box[..., 0] = (box[..., 0] - box[..., 2] / 2) * image_size[1]
-        box[..., 1] = (box[..., 1] - box[..., 3] / 2) * image_size[0]
-        box[..., 2] = (box[..., 0] + box[..., 2] / 2) * image_size[1]
-        box[..., 3] = (box[..., 1] + box[..., 3] / 2) * image_size[0]
-
-        # Extract confidence scores and class probabilities
-        box_confidence = self.sigmoid(output[..., 4:5])
-        class_probs = self.sigmoid(output[..., 5:])
-
-        boxes.append(box)
-        box_confidences.append(box_confidence)
-        box_class_probs.append(class_probs)
-
-    return boxes, box_confidences, box_class_probs
+    def process_outputs(self, outputs, image_size):
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+    
+        for output in outputs:
+            grid_height, grid_width, num_anchors, _ = output.shape
+    
+            # Reshape the output predictions
+            output = output.reshape(
+                (grid_height, grid_width, num_anchors, 4 + 1 + self.num_classes)
+            )
+    
+            # Extract bounding box coordinates
+            box = output[..., :4]
+            box[..., :2] = self.sigmoid(box[..., :2])
+            box[..., 2:] = np.exp(box[..., 2:]) * self.anchors / np.array([grid_width, grid_height])
+            box[..., 0] = (box[..., 0] + np.arange(grid_width).reshape(1, -1, 1)) / grid_width * image_size[1]
+            box[..., 1] = (box[..., 1] + np.arange(grid_height).reshape(-1, 1, 1)) / grid_height * image_size[0]
+            box[..., 2] = (box[..., 2] ** 2) * image_size[1]
+            box[..., 3] = (box[..., 3] ** 2) * image_size[0]
+    
+            # Extract confidence scores and class probabilities
+            box_confidence = self.sigmoid(output[..., 4:5])
+            class_probs = self.sigmoid(output[..., 5:])
+    
+            boxes.append(box)
+            box_confidences.append(box_confidence)
+            box_class_probs.append(class_probs)
+    
+        return boxes, box_confidences, box_class_probs
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
