923f6b759b94686c91e24e8f2f77c8b48ff85c0b
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-06-21 17:50:34 +0200

    Update 1-yolo.py

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -7,9 +7,6 @@ import numpy as np
 import tensorflow.keras as keras
 
 class Yolo:
-    """
-    yolo class
-    """
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = keras.models.load_model(model_path)
         self.class_names = self.load_class_names(classes_path)
@@ -31,32 +28,32 @@ class Yolo:
         for output in outputs:
             grid_height, grid_width, num_anchors, _ = output.shape
 
-            # Extract box coordinates, confidences, and class probabilities
-            box = output[:, :, :, :4]
-            box_confidence = output[:, :, :, 4:5]
-            box_class_prob = output[:, :, :, 5:]
+            # Extract bounding box coordinates
+            box = output[..., :4]
+            box[..., 0] = (box[..., 0] + self.sigmoid(box[..., 0])) / grid_width
+            box[..., 1] = (box[..., 1] + self.sigmoid(box[..., 1])) / grid_height
+            box[..., 2] = np.exp(box[..., 2]) * self.anchors[:, 0] / grid_width
+            box[..., 3] = np.exp(box[..., 3]) * self.anchors[:, 1] / grid_height
 
-            # Reshape box coordinates to (grid_height, grid_width, num_anchors, 4)
-            box = box.reshape((grid_height, grid_width, num_anchors, 4))
+            # Adjust bounding box coordinates to image size
+            x1 = (box[..., 0] - box[..., 2] / 2) * image_size[1]
+            y1 = (box[..., 1] - box[..., 3] / 2) * image_size[0]
+            x2 = (box[..., 0] + box[..., 2] / 2) * image_size[1]
+            y2 = (box[..., 1] + box[..., 3] / 2) * image_size[0]
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
 
-            # Calculate box coordinates relative to the original image size
-            box[..., 0:2] = sigmoid(box[..., 0:2]) + create_meshgrid(grid_width, grid_height, self.anchors)
-            box[..., 0:2] /= grid_width
-            box[..., 2:4] = np.exp(box[..., 2:4]) * self.anchors / self.model.input.shape[1].value
-            box[..., 2:4] /= image_size[::-1]
+            # Extract confidence scores and class probabilities
+            box_confidence = self.sigmoid(output[..., 4])
+            class_probs = self.sigmoid(output[..., 5:])
 
-            # Append processed outputs to the respective lists
             boxes.append(box)
             box_confidences.append(box_confidence)
-            box_class_probs.append(box_class_prob)
+            box_class_probs.append(class_probs)
 
         return boxes, box_confidences, box_class_probs
 
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
-
-def create_meshgrid(width, height, anchors):
-    x = np.tile(np.arange(0, width), height)
-    y = np.repeat(np.arange(0, height), width)
-
-    return np.stack((x, y), axis=-1).reshape((1, height, width, 1, 2))
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
