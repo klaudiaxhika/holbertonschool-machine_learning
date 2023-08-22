82faf252e7cf5cd1f5ce3469e702d0dcb849501b
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-08-23 00:12:25 +0200

    Update 1-tf_idf.py

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,11 +5,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def tf_idf(sentences, vocab=None):
     if vocab is None:
-        vectorizer = TfidfVectorizer()
-    else:
-        vectorizer = TfidfVectorizer(vocabulary=vocab)
-
+        vocab = set(word for sentence in sentences for word in sentence.split())
+    
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
     embeddings = vectorizer.fit_transform(sentences).toarray()
     features = vectorizer.get_feature_names_out()
-
+    
     return embeddings, features
