100fca76108de263ff63abda12a49ea59d09e868
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-09-11 15:20:11 +0200

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
+"""Import numpy"""
+
 import numpy as np
+"""function docs"""
+
 
 def pca(X, var=0.95):
-    # Calculate the covariance matrix of the data
+    """function docs"""
     cov_matrix = np.cov(X, rowvar=False)
-    
-    # Perform eigen decomposition on the covariance matrix
+
     eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-    
-    # Sort eigenvalues and eigenvectors in descending order
+
     sorted_indices = np.argsort(eigenvalues)[::-1]
     eigenvalues = eigenvalues[sorted_indices]
     eigenvectors = eigenvectors[:, sorted_indices]
-    
-    # Calculate the cumulative explained variance
+
     explained_variance_ratio = np.cumsum(eigenvalues) / np.sum(eigenvalues)
-    
-    # Determine the number of principal components to keep
+
     num_components = np.argmax(explained_variance_ratio >= var) + 1
-    
-    # Select the top 'num_components' eigenvectors
+
     top_eigenvectors = eigenvectors[:, :num_components]
-    
-    # Calculate the weights matrix
+
     weights_matrix = top_eigenvectors
     
     return weights_matrix
+
