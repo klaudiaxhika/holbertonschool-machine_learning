7e6ca14352686b63a3c7e3010b7abfa02615c19d
klaudiaxhika <klaudia.xhika@softup.co>
2023-09-27 23:54:15 +0200

    reg

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -3,7 +3,7 @@
 A function that creates the training operation for a neural network
 in tensorflow using the Adam optimization algorithm
 """
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
