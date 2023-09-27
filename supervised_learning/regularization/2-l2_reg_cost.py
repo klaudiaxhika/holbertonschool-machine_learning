34eb3d98466314fe016a2d1fa2bbe7a08eb24992
klaudiaxhika <klaudia.xhika@softup.co>
2023-09-27 23:32:46 +0200

    reg

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""Imports tensorflow"""
-
-
+"""A function that calculates the cost of a neural network
+with L2 regularization"""
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, lambtha):
+def l2_reg_cost(cost):
     """
-    Calculates the cost of a neural network with L2 regularization
-    """
-    l2_reg_cost = tf.losses.get_regularization_losses()
-    return (cost + l2_reg_cost)
+    A function that calculates the cost of a neural network
+    with l2 regularization"""
+    return cost + tf.losses.get_regularization_losses()
