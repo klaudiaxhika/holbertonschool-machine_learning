1841be6c2573abf31ab4aab2107560f140287243
klaudiaxhika <klaudia.xhika@softup.co>
2023-09-27 23:55:52 +0200

    reg

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -3,7 +3,7 @@
 A function that creates the training operation for a neural network in
 tensorflow using the gradient descent with momentum optimization algorithm
 """
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
