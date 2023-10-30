0349934d1badb6778ff4d12e434d25504d81a3a1
klaudiaxhika <107842396+klaudiaxhika@users.noreply.github.com>
2023-10-30 12:03:51 +0100

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python3
 """Pipeline Api"""
-import sys
 import requests
-import time
+import sys
+from datetime import datetime
 
-def get_user_location(api_url):
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            user_data = response.json()
-            if user_data.get("location"):
-                return user_data["location"]
-            else:
-                return "Location not specified"
-        elif response.status_code == 404:
-            return "Not found"
-        elif response.status_code == 403:
-            reset_time = int(response.headers.get("X-Ratelimit-Reset"))
-            current_time = int(time.time())
-            reset_in_minutes = (reset_time - current_time) // 60
-            return f"Reset in {reset_in_minutes} min"
-        else:
-            return "Unexpected error"
-    except requests.exceptions.RequestException as e:
-        return str(e)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
+    """pipeline api"""
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        string = 'X-Ratelimit-Reset'
+        date = datetime.fromtimestamp(int(response.headers[string]))
+        min = str((date - datetime.now())).split(':')[1]
+        min = int(min)
+        print("Reset in {} min".format(min))
     else:
-        api_url = sys.argv[1]
-        location = get_user_location(api_url)
-        print(location)
+        print(response.json()["location"])
